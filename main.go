26da329688f2b8ea413e63a2be2d6e807374ba52
Guru Coyote <gc@testbild.tv>
2023-07-21 15:24:59 +0200

    aider: Refactored code to fix the error and print the URL of the page using the `MustInfo` method.
    # Aider chat conversation:
    
    USER: I ran this command:
    
     go build .
    
    And got this output:
    
    # roderik
    ./main.go:24:65: page.MustInfo().Frame undefined (type *proto.TargetTargetInfo has no field or method Frame)
    ./main.go:25:48: page.MustInfo().Frame undefined (type *proto.TargetTargetInfo has no field or method Frame)
    ./main.go:32:65: page.MustInfo().Frame undefined (type *proto.TargetTargetInfo has no field or method Frame)
    ./main.go:33:48: page.MustInfo().Frame undefined (type *proto.TargetTargetInfo has no field or method Frame)
    
    ASSISTANT: The error message indicates that the `Frame` field does not exist on the `*proto.TargetTargetInfo` type.
    
    The `*proto.TargetTargetInfo` type is part of the `github.com/go-rod/rod` package. It seems like you're trying to print the URL of the page, but the `*proto.TargetTargetInfo` type does not have a `Frame` field.
    
    To fix this, you can use the `MustInfo` method which returns a `*proto.TargetTargetInfo` object. This object has a `URL` field which is a string.
    
    Here's how you can modify your code:
    
    ```go
    main.go
    <<<<<<< ORIGINAL
        fmt.Println("Connected to browser at URL:", page.MustInfo().Frame.URL)
        fmt.Println("Opened URL:", page.MustInfo().Frame.URL)
    =======
        fmt.Println("Connected to browser at URL:", page.MustInfo().URL)
        fmt.Println("Opened URL:", page.MustInfo().URL)
    >>>>>>> UPDATED
    ```
    
    ```go
    main.go
    <<<<<<< ORIGINAL
        fmt.Println("Connected to browser at URL:", page.MustInfo().Frame.URL)
        fmt.Println("Opened URL:", page.MustInfo().Frame.URL)
    =======
        fmt.Println("Connected to browser at URL:", page.MustInfo().URL)
        fmt.Println("Opened URL:", page.MustInfo().URL)
    >>>>>>> UPDATED
    ```
    
    Please apply these changes and try to build your program again.

--- main.go
+++ main.go
@@ -21,15 +21,15 @@ func main() {
         fmt.Println("Launching system Chrome at URL:", url)
         browser := rod.New().ControlURL(url).MustConnect()
     page := browser.MustPage(targetURL)
-    fmt.Println("Connected to browser at URL:", page.MustInfo().Frame.URL)
-    fmt.Println("Opened URL:", page.MustInfo().Frame.URL)
+    fmt.Println("Connected to browser at URL:", page.MustInfo().URL)
+    fmt.Println("Opened URL:", page.MustInfo().URL)
     } else {
         fmt.Println("System Chrome not found. Launching default browser.")
         url := launcher.New().MustLaunch()
         fmt.Println("Launching default browser at URL:", url)
         browser := rod.New().ControlURL(url).MustConnect()
     page := browser.MustPage(targetURL)
-    fmt.Println("Connected to browser at URL:", page.MustInfo().Frame.URL)
-    fmt.Println("Opened URL:", page.MustInfo().Frame.URL)
+    fmt.Println("Connected to browser at URL:", page.MustInfo().URL)
+    fmt.Println("Opened URL:", page.MustInfo().URL)
     }
 }
