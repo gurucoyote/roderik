fea24f023dcf8f8ec5169509cede56540c538cc0
Guru Coyote <gc@testbild.tv>
2023-07-21 15:24:59 +0200

    aider: Created two stub commands `click` and `type` in the `cmd/interact.go` file.
    # Aider chat conversation:
    
    USER: in this new file, please create two stub commands: click and type. The type command should take any number of extra arguments, which will be used as the text to type
    
    ASSISTANT: Sure, I will create two stub commands `click` and `type` in the `cmd/interact.go` file.
    
    Here is the content for the new file:
    
    ```go
    cmd/interact.go
    <<<<<<< ORIGINAL
    package cmd
    
    // Add your code here
    =======
    package cmd
    
    import (
    	"github.com/spf13/cobra"
    )
    
    var ClickCmd = &cobra.Command{
    	Use:   "click",
    	Short: "Click on the current element",
    	Run: func(cmd *cobra.Command, args []string) {
    		// Add your code here
    	},
    }
    
    var TypeCmd = &cobra.Command{
    	Use:   "type",
    	Short: "Type text into the current element",
    	Run: func(cmd *cobra.Command, args []string) {
    		// Add your code here
    	},
    }
    
    func init() {
    	RootCmd.AddCommand(ClickCmd)
    	RootCmd.AddCommand(TypeCmd)
    }
    >>>>>>> UPDATED
    ```
    
    Please let me know if you need further changes.

--- cmd/interact.go
+++ cmd/interact.go
@@ -1,3 +1,26 @@
 package cmd
 
-// Add your code here
+import (
+	"github.com/spf13/cobra"
+)
+
+var ClickCmd = &cobra.Command{
+	Use:   "click",
+	Short: "Click on the current element",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Add your code here
+	},
+}
+
+var TypeCmd = &cobra.Command{
+	Use:   "type",
+	Short: "Type text into the current element",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Add your code here
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(ClickCmd)
+	RootCmd.AddCommand(TypeCmd)
+}
