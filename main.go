142251abd46c6d01bb02dd0e3712aaea8313000b
Guru Coyote <gc@testbild.tv>
2023-07-21 15:24:59 +0200

    Refactor main.go to ensure user data directory exists and get the browser executable path.
    # Aider chat conversation:

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/launcher"
-	"github.com/go-rod/rod/lib/utils"
+	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -24,31 +22,21 @@ func main() {
 		os.Mkdir(userDataDir, 0755)
 	}
 
-    // Ensure user data directory exists
-    userDataDir = filepath.Join(".", "user_data")
-    if _, err := os.Stat(userDataDir); os.IsNotExist(err) {
-        os.Mkdir(userDataDir, 0755)
-    }
+	// Ensure user data directory exists
+	userDataDir = filepath.Join(".", "user_data")
+	if _, err := os.Stat(userDataDir); os.IsNotExist(err) {
+		os.Mkdir(userDataDir, 0755)
+	}
 
-    // Get the browser executable path
-    path, exists := launcher.LookPath()
-    if !exists {
-        fmt.Println("Browser executable not found.")
-        os.Exit(1)
-    }
-
-    // use the FormatArgs to construct args, this line is optional, you can construct the args manually
-    l := launcher.New().Bin(path).UserDataDir(userDataDir)
-    args := l.FormatArgs()
-
-    var cmd *exec.Cmd
-    cmd = exec.Command(path, args...)
-
-    parser := launcher.NewURLParser()
-    cmd.Stderr = parser
-    utils.E(cmd.Start())
-    u := launcher.MustResolveURL(<-parser.URL)
-
+	// Get the browser executable path
+	path, _ := launcher.LookPath()
+	u := launcher.New().Bin(path).
+		Set("disable-web-security").
+		Set("disable-setuid-sandbox").
+		Set("no-sandbox").
+		Set("no-first-run", "true").
+		Set("disable-gpu").
+		Headless(true).MustLaunch()
 	browser := rod.New().ControlURL(u).MustConnect()
 
 	page := browser.MustPage(targetURL)
