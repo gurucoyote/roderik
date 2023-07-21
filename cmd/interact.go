cdd76ab97dd0683bb1dd8ea5002ec8697ba6eaff
Guru Coyote <gc@testbild.tv>
2023-07-21 15:24:59 +0200

    aider: Created 'rclick' sub command to right click on the current element.
    # Aider chat conversation:
    
    USER: please create a 'rclick' sub command along the lines of the 'click', using the right mouse button accordingly
    
    ASSISTANT: Sure, I will create a 'rclick' sub command in the `cmd/interact.go` file.
    
    Here is the change:
    
    ```go
    cmd/interact.go
    <<<<<<< ORIGINAL
    var TypeCmd = &cobra.Command{
    	Use:   "type",
    	Short: "Type text into the current element",
    	Run: func(cmd *cobra.Command, args []string) {
    		// Add your code here
    	},
    }
    
    func init() {
    	RootCmd.AddCommand(ClickCmd)
    	RootCmd.AddCommand(TypeCmd)
    }
    =======
    var RClickCmd = &cobra.Command{
    	Use:   "rclick",
    	Short: "Right click on the current element",
    	Run: func(cmd *cobra.Command, args []string) {
    		if !hasCurrentElement() {
    			return
    		}
    		err := CurrentElement.Click(proto.InputMouseButtonRight, 1)
    		if err != nil {
    			fmt.Println("Error right clicking on the current element:", err)
    			return
    		}
    	},
    }
    
    var TypeCmd = &cobra.Command{
    	Use:   "type",
    	Short: "Type text into the current element",
    	Run: func(cmd *cobra.Command, args []string) {
    		// Add your code here
    	},
    }
    
    func init() {
    	RootCmd.AddCommand(ClickCmd)
    	RootCmd.AddCommand(RClickCmd)
    	RootCmd.AddCommand(TypeCmd)
    }
    >>>>>>> UPDATED
    ```
    
    Please let me know if you need further changes.

--- cmd/interact.go
+++ cmd/interact.go
@@ -21,6 +21,21 @@ var ClickCmd = &cobra.Command{
 	},
 }
 
+var RClickCmd = &cobra.Command{
+	Use:   "rclick",
+	Short: "Right click on the current element",
+	Run: func(cmd *cobra.Command, args []string) {
+		if !hasCurrentElement() {
+			return
+		}
+		err := CurrentElement.Click(proto.InputMouseButtonRight, 1)
+		if err != nil {
+			fmt.Println("Error right clicking on the current element:", err)
+			return
+		}
+	},
+}
+
 var TypeCmd = &cobra.Command{
 	Use:   "type",
 	Short: "Type text into the current element",
@@ -31,5 +46,6 @@ var TypeCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(ClickCmd)
+	RootCmd.AddCommand(RClickCmd)
 	RootCmd.AddCommand(TypeCmd)
 }
