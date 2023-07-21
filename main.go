9153946e91ef3d39c07c4334664cf0904fe44127
Guru Coyote <gc@testbild.tv>
2023-07-21 15:24:59 +0200

    Ensure user data directory exists and launch browser at target URL.
    # Aider chat conversation:

--- main.go
+++ main.go
@@ -1,35 +1,57 @@
 package main
+
 import (
-    "fmt"
-    "os"
-    "github.com/go-rod/rod"
-    "github.com/go-rod/rod/lib/launcher"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
+	"github.com/go-rod/rod/lib/utils"
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Please provide a URL as a command-line argument.")
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a URL as a command-line argument.")
+		os.Exit(1)
+	}
+	targetURL := os.Args[1]
+	fmt.Println("Target URL:", targetURL)
+
+	// Ensure user data directory exists
+	userDataDir := filepath.Join(".", "user_data")
+	if _, err := os.Stat(userDataDir); os.IsNotExist(err) {
+		os.Mkdir(userDataDir, 0755)
+	}
+
+    // Ensure user data directory exists
+    userDataDir = filepath.Join(".", "user_data")
+    if _, err := os.Stat(userDataDir); os.IsNotExist(err) {
+        os.Mkdir(userDataDir, 0755)
+    }
+
+    // Get the browser executable path
+    path, exists := launcher.LookPath()
+    if !exists {
+        fmt.Println("Browser executable not found.")
         os.Exit(1)
     }
-    targetURL := os.Args[1]
-    fmt.Println("Target URL:", targetURL)
 
-    path, exists := launcher.LookPath()
-    if exists {
-        fmt.Println("System Chrome found at:", path)
-        url := launcher.New().Bin(path).MustLaunch()
-        fmt.Println("Launching system Chrome at URL:", url)
-        browser := rod.New().ControlURL(url).MustConnect()
-    page := browser.MustPage(targetURL)
-    fmt.Println("Connected to browser at URL:", page.MustInfo().URL)
-    fmt.Println("Opened URL:", page.MustInfo().URL)
-    } else {
-        fmt.Println("System Chrome not found. Launching default browser.")
-        url := launcher.New().MustLaunch()
-        fmt.Println("Launching default browser at URL:", url)
-        browser := rod.New().ControlURL(url).MustConnect()
-    page := browser.MustPage(targetURL)
-    fmt.Println("Connected to browser at URL:", page.MustInfo().URL)
-    fmt.Println("Opened URL:", page.MustInfo().URL)
-    }
+    // use the FormatArgs to construct args, this line is optional, you can construct the args manually
+    l := launcher.New().Bin(path).UserDataDir(userDataDir)
+    args := l.FormatArgs()
+
+    var cmd *exec.Cmd
+    cmd = exec.Command(path, args...)
+
+    parser := launcher.NewURLParser()
+    cmd.Stderr = parser
+    utils.E(cmd.Start())
+    u := launcher.MustResolveURL(<-parser.URL)
+
+	browser := rod.New().ControlURL(u).MustConnect()
+
+	page := browser.MustPage(targetURL)
+	fmt.Println("Connected to browser at URL:", page.MustInfo().URL)
+	fmt.Println("Opened URL:", page.MustInfo().URL)
 }
