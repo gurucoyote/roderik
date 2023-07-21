63db7d9821b9f5094cc2d83dea260b6ba1eba262
Guru Coyote <gc@testbild.tv>
2023-07-21 15:24:59 +0200

    Added command-line argument to specify target URL and added logic to launch system Chrome if available, otherwise launch default browser.
    # Aider chat conversation:

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
+package main
 import (
+    "fmt"
+    "os"
     "github.com/go-rod/rod"
     "github.com/go-rod/rod/lib/launcher"
 )
 
 func main() {
-    url := launcher.New().MustLaunch()
-    browser := rod.New().ControlURL(url).MustConnect()
+    if len(os.Args) < 2 {
+        fmt.Println("Please provide a URL as a command-line argument.")
+        os.Exit(1)
+    }
+    targetURL := os.Args[1]
+    fmt.Println("Target URL:", targetURL)
+
+    path, exists := launcher.LookPath()
+    if exists {
+        fmt.Println("System Chrome found at:", path)
+        url := launcher.New().Bin(path).MustLaunch()
+        fmt.Println("Launching system Chrome at URL:", url)
+        browser := rod.New().ControlURL(url).MustConnect()
+        fmt.Println("Connected to browser at URL:", browser.URL)
+        page := browser.MustPage(targetURL)
+        fmt.Println("Opened URL:", page.MustInfo().URL)
+    } else {
+        fmt.Println("System Chrome not found. Launching default browser.")
+        url := launcher.New().MustLaunch()
+        fmt.Println("Launching default browser at URL:", url)
+        browser := rod.New().ControlURL(url).MustConnect()
+        fmt.Println("Connected to browser at URL:", browser.URL)
+        page := browser.MustPage(targetURL)
+        fmt.Println("Opened URL:", page.MustInfo().URL)
+    }
 }
