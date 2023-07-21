8d1dc42f34600731d418fffc2e0401048a879d6c
Guru Coyote <gc@testbild.tv>
2023-07-21 15:24:59 +0200

    Refactor prepareBrowserAndLoadURL and reportOnHeadings functions, and remove unused imports and functions.
    # Aider chat conversation:

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
 	"github.com/spf13/cobra"
 )
 
@@ -30,4 +35,60 @@ var RootCmd = &cobra.Command{
 
 		reportOnHeadings(page)
 	},
+}
+func prepareBrowserAndLoadURL(targetURL string) *rod.Page {
+	// Ensure user data directory exists
+	userDataDir := filepath.Join(".", "user_data")
+	if _, err := os.Stat(userDataDir); os.IsNotExist(err) {
+		os.Mkdir(userDataDir, 0755)
+	}
+
+	// Get the browser executable path
+	path, _ := launcher.LookPath()
+	u := launcher.New().Bin(path).
+		Set("disable-web-security").
+		Set("disable-setuid-sandbox").
+		Set("no-sandbox").
+		Set("no-first-run", "true").
+		Set("disable-gpu").
+		Set("user-data-dir", userDataDir).
+		Headless(true).MustLaunch()
+	browser := rod.New().ControlURL(u).MustConnect()
+
+	return browser.MustPage(targetURL).MustWaitLoad()
+}
+
+func reportOnHeadings(page *rod.Page) {
+	// Get all headings
+	headings := page.MustElements("h1, h2, h3, h4, h5, h6")
+
+	// Print the count of headings
+	fmt.Println("Count of headings:", len(headings))
+
+	// Print each heading
+	for _, heading := range headings {
+		// Get the heading level
+		level := heading.MustEval("() => this.tagName").String() // [1]
+		// fmt.Println(level)
+
+		// Get the heading text
+		text := heading.MustText()
+
+		fmt.Printf("%s: %s\n", level, text)
+	}
+
+	// Output the description and font-family of the first heading element
+	if len(headings) > 0 {
+		firstHeading := headings[0]
+		fontFamily := firstHeading.MustEval(`() => getComputedStyle(this).fontFamily`).String()
+		fmt.Println("Font Family of the first heading:", fontFamily)
+		computedStyles := firstHeading.MustEval(`() => getComputedStyle(this)`)
+		fmt.Println("computed styles", PrettyFormat(computedStyles))
+		// description := firstHeading.MustDescribe()
+		// fmt.Println("Description: ", PrettyFormat(description))
+	}
+}
+func PrettyFormat(v interface{}) string {
+	b, _ := json.MarshalIndent(v, "", "  ")
+return string(b)
 }
--- main.go
+++ main.go
@@ -4,11 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"path/filepath"
-
-	"github.com/go-rod/rod"
-	"github.com/go-rod/rod/lib/launcher"
-	"github.com/spf13/cobra"
 	"roderik/cmd"
 )
 
@@ -28,10 +23,6 @@ func main() {
 	}
 }
 // PrettyFormat function
-func PrettyFormat(v interface{}) string {
-	b, _ := json.MarshalIndent(v, "", "  ")
-return string(b)
-}
 
 // prettyPrintJson function
 func prettyPrintJson(s string) string {
@@ -45,57 +36,5 @@ func main() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}
-}
-func prepareBrowserAndLoadURL(targetURL string) *rod.Page {
-	// Ensure user data directory exists
-	userDataDir := filepath.Join(".", "user_data")
-	if _, err := os.Stat(userDataDir); os.IsNotExist(err) {
-		os.Mkdir(userDataDir, 0755)
-	}
-
-	// Get the browser executable path
-	path, _ := launcher.LookPath()
-	u := launcher.New().Bin(path).
-		Set("disable-web-security").
-		Set("disable-setuid-sandbox").
-		Set("no-sandbox").
-		Set("no-first-run", "true").
-		Set("disable-gpu").
-		Set("user-data-dir", userDataDir).
-		Headless(true).MustLaunch()
-	browser := rod.New().ControlURL(u).MustConnect()
-
-	return browser.MustPage(targetURL).MustWaitLoad()
-}
-
-func reportOnHeadings(page *rod.Page) {
-	// Get all headings
-	headings := page.MustElements("h1, h2, h3, h4, h5, h6")
-
-	// Print the count of headings
-	fmt.Println("Count of headings:", len(headings))
-
-	// Print each heading
-	for _, heading := range headings {
-		// Get the heading level
-		level := heading.MustEval("() => this.tagName").String() // [1]
-		// fmt.Println(level)
-
-		// Get the heading text
-		text := heading.MustText()
-
-		fmt.Printf("%s: %s\n", level, text)
-	}
-
-	// Output the description and font-family of the first heading element
-	if len(headings) > 0 {
-		firstHeading := headings[0]
-		fontFamily := firstHeading.MustEval(`() => getComputedStyle(this).fontFamily`).String()
-		fmt.Println("Font Family of the first heading:", fontFamily)
-		computedStyles := firstHeading.MustEval(`() => getComputedStyle(this)`)
-		fmt.Println("computed styles", PrettyFormat(computedStyles))
-		// description := firstHeading.MustDescribe()
-		// fmt.Println("Description: ", PrettyFormat(description))
 	}
 }
