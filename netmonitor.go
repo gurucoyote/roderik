0e32e6f56bf7a6d6f1707af93a256c90704ad23f
Guru Coyote <gc@testbild.tv>
2023-09-23 21:03:08 +0200

    aider: Created a new file `netmonitor.go` and moved the code into this file.
    # Aider chat conversation:
    
    USER: Please create a new file netmonitor.go and move the code just created into this file
    
    ASSISTANT: Sure, I will create a new file named `netmonitor.go` and move the code into this file. Here is the *edit block* for this operation:
    
    ```go
    netmonitor.go
    <<<<<<< HEAD
    
    =======
    package main
    
    import (
    	"fmt"
    	"sync"
    
    	"github.com/go-rod/rod"
    	"github.com/go-rod/rod/lib/proto"
    )
    
    type EventLog struct {
    	mu    sync.Mutex
    	logs  []string
    }
    
    func (l *EventLog) Add(log string) {
    	l.mu.Lock()
    	defer l.mu.Unlock()
    	l.logs = append(l.logs, log)
    }
    
    func (l *EventLog) Display() {
    	l.mu.Lock()
    	defer l.mu.Unlock()
    	for _, log := range l.logs {
    		fmt.Println(log)
    	}
    }
    
    func main() {
    	browser := rod.New().MustConnect()
    	defer browser.MustClose()
    
    	page := browser.MustPage()
    
    	eventLog := &EventLog{}
    
    	page.EnableDomain(proto.NetworkEnable{})
    	go page.EachEvent(func(e *proto.NetworkRequestWillBeSent) {
    		eventLog.Add(fmt.Sprintf("Request sent: %s", e.Request.URL))
    	})()
    	go page.EachEvent(func(e *proto.NetworkResponseReceived) {
    		eventLog.Add(fmt.Sprintf("Response received: %s Status: %d", e.Response.URL, e.Response.Status))
    	})()
    
    	page.MustNavigate("https://example.com")
    
    	// Display the logs
    	eventLog.Display()
    }
    >>>>>>> updated
    ```
    
    This *edit block* creates a new file `netmonitor.go` and adds the provided code into it.

---
+++ netmonitor.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/proto"
+)
+
+type EventLog struct {
+	mu    sync.Mutex
+	logs  []string
+}
+
+func (l *EventLog) Add(log string) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.logs = append(l.logs, log)
+}
+
+func (l *EventLog) Display() {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	for _, log := range l.logs {
+		fmt.Println(log)
+	}
+}
+
+func main() {
+	browser := rod.New().MustConnect()
+	defer browser.MustClose()
+
+	page := browser.MustPage()
+
+	eventLog := &EventLog{}
+
+	page.EnableDomain(proto.NetworkEnable{})
+	go page.EachEvent(func(e *proto.NetworkRequestWillBeSent) {
+		eventLog.Add(fmt.Sprintf("Request sent: %s", e.Request.URL))
+	})()
+	go page.EachEvent(func(e *proto.NetworkResponseReceived) {
+		eventLog.Add(fmt.Sprintf("Response received: %s Status: %d", e.Response.URL, e.Response.Status))
+	})()
+
+	page.MustNavigate("https://example.com")
+
+	// Display the logs
+	eventLog.Display()
+}
