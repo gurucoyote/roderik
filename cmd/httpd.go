5da2b248113d3ba34df6913fa6b323554c2255a8
Guru Coyote <gc@testbild.tv>
2023-09-25 18:37:44 +0200

    aider: Added a new command line flag `-a` or `--basic-auth` that requires the server to authenticate using Basic Authentication with username 'user' and password 'pass'.
    # Aider chat conversation:
    
    ASSISTANT: To implement this feature, we need to modify the `cmd/httpd.go` file. We will add a new flag `basicAuth` to the `init` function. Then, in the `Run` function of `serverCmd`, we will check if this flag is set. If it is, we will wrap the file server handler with a function that checks the `Authorization` header of each request.
    
    Here are the changes:
    
    ```go
    cmd/httpd.go
    <<<<<<< HEAD
    import (
    	"flag"
    	"fmt"
    	"github.com/spf13/cobra"
    	"net/http"
    )
    
    var port int
    
    var serverCmd = &cobra.Command{
    	Use:   "server",
    	Short: "Start a simple static file server",
    	Run: func(cmd *cobra.Command, args []string) {
    		fs := http.FileServer(http.Dir("assets/"))
    		http.Handle("/", fs)
    
    		addr := fmt.Sprintf(":%d", port)
    		fmt.Printf("Starting server on %s\n", addr)
    		if err := http.ListenAndServe(addr, nil); err != nil {
    			panic(err)
    		}
    	},
    }
    
    func init() {
    	flag.IntVar(&port, "port", 80, "Port to run the server on")
    	RootCmd.AddCommand(serverCmd)
    }
    =======
    import (
    	"encoding/base64"
    	"flag"
    	"fmt"
    	"github.com/spf13/cobra"
    	"net/http"
    	"strings"
    )
    
    var (
    	port      int
    	basicAuth bool
    )
    
    var serverCmd = &cobra.Command{
    	Use:   "server",
    	Short: "Start a simple static file server",
    	Run: func(cmd *cobra.Command, args []string) {
    		fs := http.FileServer(http.Dir("assets/"))
    		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
    			if basicAuth {
    				auth := r.Header.Get("Authorization")
    				if auth == "" {
    					w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
    					http.Error(w, "Unauthorized.", http.StatusUnauthorized)
    					return
    				}
    
    				payload, _ := base64.StdEncoding.DecodeString(strings.TrimPrefix(auth, "Basic "))
    				pair := strings.SplitN(string(payload), ":", 2)
    
    				if len(pair) != 2 || !(pair[0] == "user" && pair[1] == "pass") {
    					http.Error(w, "Unauthorized.", http.StatusUnauthorized)
    					return
    				}
    			}
    
    			fs.ServeHTTP(w, r)
    		})
    
    		http.Handle("/", handler)
    
    		addr := fmt.Sprintf(":%d", port)
    		fmt.Printf("Starting server on %s\n", addr)
    		if err := http.ListenAndServe(addr, nil); err != nil {
    			panic(err)
    		}
    	},
    }
    
    func init() {
    	flag.IntVar(&port, "port", 80, "Port to run the server on")
    	flag.BoolVar(&basicAuth, "basic-auth", false, "Require basic auth")
    	RootCmd.AddCommand(serverCmd)
    }
    >>>>>>> updated
    ```
    
    This change adds a new command line flag `-a` or `--basic-auth` that, when set, will require the server to authenticate using Basic Authentication with username 'user' and password 'pass'.

--- cmd/httpd.go
+++ cmd/httpd.go
@@ -1,20 +1,46 @@
 package cmd
 
 import (
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"github.com/spf13/cobra"
 	"net/http"
+	"strings"
 )
 
-var port int
+var (
+	port      int
+	basicAuth bool
+)
 
 var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "Start a simple static file server",
 	Run: func(cmd *cobra.Command, args []string) {
 		fs := http.FileServer(http.Dir("assets/"))
-		http.Handle("/", fs)
+		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if basicAuth {
+				auth := r.Header.Get("Authorization")
+				if auth == "" {
+					w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+					http.Error(w, "Unauthorized.", http.StatusUnauthorized)
+					return
+				}
+
+				payload, _ := base64.StdEncoding.DecodeString(strings.TrimPrefix(auth, "Basic "))
+				pair := strings.SplitN(string(payload), ":", 2)
+
+				if len(pair) != 2 || !(pair[0] == "user" && pair[1] == "pass") {
+					http.Error(w, "Unauthorized.", http.StatusUnauthorized)
+					return
+				}
+			}
+
+			fs.ServeHTTP(w, r)
+		})
+
+		http.Handle("/", handler)
 
 		addr := fmt.Sprintf(":%d", port)
 		fmt.Printf("Starting server on %s\n", addr)
@@ -26,5 +52,6 @@ var serverCmd = &cobra.Command{
 
 func init() {
 	flag.IntVar(&port, "port", 80, "Port to run the server on")
+	flag.BoolVar(&basicAuth, "basic-auth", false, "Require basic auth")
 	RootCmd.AddCommand(serverCmd)
 }
