77e988231bd0074fd7f8062c742cb3d5d36a1ff3
Guru Coyote <gc@testbild.tv>
2023-09-26 14:52:13 +0200

    Refactor printing of computed string and sources for node name in a11y command.

--- cmd/a11y.go
+++ cmd/a11y.go
@@ -50,14 +50,16 @@ var A11yCmd = &cobra.Command{
 			fmt.Println("Node ID:", node.NodeID)
 			fmt.Println("Role:", node.Role.Value)
 			if node.Name != nil {
-				fmt.Println("Computed string:", node.Name.Value)
-				for _, source := range node.Name.Sources {
-					fmt.Println("Source:", source.Type)
-					if source.Attribute != "" {
-						fmt.Println("Attribute:", source.Attribute)
-					}
-					if source.Value != nil {
-						fmt.Println("Value:", source.Value.Value)
+				fmt.Println("Name:", node.Name.Value)
+				if false {
+					for _, source := range node.Name.Sources {
+						fmt.Println("Source:", source.Type)
+						if source.Attribute != "" {
+							fmt.Println("Attribute:", source.Attribute)
+						}
+						if source.Value != nil {
+							fmt.Println("Value:", source.Value.Value)
+						}
 					}
 				}
 			}
