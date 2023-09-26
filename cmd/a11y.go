957addab84b66c39b914f98bcca3af56b8b4a307
Guru Coyote <gc@testbild.tv>
2023-09-26 13:51:36 +0200

    * some manual tweak for better logic and usability

--- cmd/a11y.go
+++ cmd/a11y.go
@@ -20,60 +20,59 @@ var A11yCmd = &cobra.Command{
 					return
 				}
 				CurrentElement = Page.MustElement("body")
-				// Get the element's properties
-				elementProperties, err := CurrentElement.Describe(0, false) // depth:0, pierce:false
-				if err != nil {
-					fmt.Println("Error describing element:", err)
-					return
-				}
-
-				// Fetch the partial accessibility tree for this element
-				partialAXTree, err := proto.AccessibilityGetPartialAXTree{
-					BackendNodeID: elementProperties.BackendNodeID,
-				}.Call(Page)
-				if err != nil {
-					fmt.Println("Error fetching partial accessibility tree:", err)
-					return
-				}
-				// Iterate over the Nodes of the partial tree and output relevant info
-				for _, node := range partialAXTree.Nodes {
-					// Filter out any nodes that have ignore set to true
-					if node.Ignored {
-						continue
-					}
-					// Relevant info: computed string as text, source, number of children, ids and role
-					fmt.Println("Node ID:", node.NodeID)
-					fmt.Println("Role:", node.Role.Value)
-					if node.Name != nil {
-						fmt.Println("Computed string:", node.Name.Value)
-						for _, source := range node.Name.Sources {
-							fmt.Println("Source:", source.Type)
-							if source.Attribute != "" {
-								fmt.Println("Attribute:", source.Attribute)
-							}
-							if source.Value != nil {
-								fmt.Println("Value:", source.Value.Value)
-							}
-						}
-					}
-					fmt.Println("Number of children:", len(node.ChildIds))
-					fmt.Println("Child IDs:", node.ChildIds)
-				}
-				if Verbose {
-					// debug: print the tree as json
-					treeJSON, err := json.MarshalIndent(partialAXTree, "", "  ")
-					if err != nil {
-						fmt.Println("Error converting node to JSON:", err)
-						return
-					}
-					fmt.Println(string(treeJSON))
-				}
 			}
 		}
 		if !hasCurrentElement() {
 			return
 		}
-		// ReportElement(CurrentElement)
+		// Get the element's properties
+		elementProperties, err := CurrentElement.Describe(0, false) // depth:0, pierce:false
+		if err != nil {
+			fmt.Println("Error describing element:", err)
+			return
+		}
+
+		// Fetch the partial accessibility tree for this element
+		partialAXTree, err := proto.AccessibilityGetPartialAXTree{
+			BackendNodeID: elementProperties.BackendNodeID,
+		}.Call(Page)
+		if err != nil {
+			fmt.Println("Error fetching partial accessibility tree:", err)
+			return
+		}
+		// Iterate over the Nodes of the partial tree and output relevant info
+		for _, node := range partialAXTree.Nodes {
+			// Filter out any nodes that have ignore set to true
+			if node.Ignored {
+				continue
+			}
+			// Relevant info: computed string as text, source, number of children, ids and role
+			fmt.Println("Node ID:", node.NodeID)
+			fmt.Println("Role:", node.Role.Value)
+			if node.Name != nil {
+				fmt.Println("Computed string:", node.Name.Value)
+				for _, source := range node.Name.Sources {
+					fmt.Println("Source:", source.Type)
+					if source.Attribute != "" {
+						fmt.Println("Attribute:", source.Attribute)
+					}
+					if source.Value != nil {
+						fmt.Println("Value:", source.Value.Value)
+					}
+				}
+			}
+			fmt.Println("Number of children:", len(node.ChildIds))
+			fmt.Println("Child IDs:", node.ChildIds)
+		}
+		if Verbose {
+			// debug: print the tree as json
+			treeJSON, err := json.MarshalIndent(partialAXTree, "", "  ")
+			if err != nil {
+				fmt.Println("Error converting node to JSON:", err)
+				return
+			}
+			fmt.Println(string(treeJSON))
+		}
 	},
 }
 
