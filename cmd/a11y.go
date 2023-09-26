19cfb4d2a3bc0bcd445da9d6c5cf73c1e714583b
Guru Coyote <gc@testbild.tv>
2023-09-26 09:45:17 +0200

    Added functionality to load a URL and set the current element to the body element if a valid URL is provided as an argument.

--- cmd/a11y.go
+++ cmd/a11y.go
@@ -3,13 +3,23 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"github.com/go-rod/rod"
 )
 
 var A11yCmd = &cobra.Command{
 	Use:   "a11y",
 	Short: "Access the accessibility tree of the current element",
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) > 0 {
+			url := args[0]
+			if isValidURL(url) {
+				Page, err := LoadURL(url)
+				if err != nil {
+					fmt.Println("Error loading URL:", err)
+					return
+				}
+				CurrentElement = Page.MustElement("body")
+			}
+		}
 		if !hasCurrentElement() {
 			return
 		}
