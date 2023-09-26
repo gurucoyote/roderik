2b89b051497c79b1b4f53f215fda9f696c7dd5f6
Guru Coyote <gc@testbild.tv>
2023-09-26 12:48:20 +0200

    Refactor code to fetch partial accessibility tree for the current element and output relevant information.

--- cmd/a11y.go
+++ cmd/a11y.go
@@ -21,21 +21,13 @@ var A11yCmd = &cobra.Command{
 				}
 				CurrentElement = Page.MustElement("body")
 				// Get the element's properties
-				elementProperties, err := CurrentElement.Describe(0, false)
+				elementProperties, err := CurrentElement.Describe(0, false) // depth:0, pierce:false
 				if err != nil {
 					fmt.Println("Error describing element:", err)
 					return
 				}
-				// Convert the element properties to an indented JSON string
-				elementPropertiesJSON, err := json.MarshalIndent(elementProperties, "", "  ")
-				if err != nil {
-					fmt.Println("Error converting element properties to JSON:", err)
-					return
-				}
-				// Output the element properties
-				fmt.Println(string(elementPropertiesJSON))
 
-				// Fetch the partial accessibility tree
+				// Fetch the partial accessibility tree for this element
 				partialAXTree, err := proto.AccessibilityGetPartialAXTree{
 					BackendNodeID: elementProperties.BackendNodeID,
 				}.Call(Page)
@@ -43,18 +35,23 @@ var A11yCmd = &cobra.Command{
 					fmt.Println("Error fetching partial accessibility tree:", err)
 					return
 				}
-				// Debug: Iterate over the nodes in the partial accessibility tree and print their properties
-				for i, node := range partialAXTree.Nodes {
-					fmt.Printf("Node %d:\n", i)
-					nodeJSON, err := json.MarshalIndent(node, "", "  ")
+				if false {
+				// debug: print the tree as json
+					treeJSON, err := json.MarshalIndent(partialAXTree, "", "  ")
 					if err != nil {
 						fmt.Println("Error converting node to JSON:", err)
 						return
 					}
-					fmt.Println(string(nodeJSON))
+					fmt.Println(string(treeJSON))
 				}
 			}
 		}
+		//TODO: iterate over the Nodes of the partial tree and output relevant info
+		// filter out any nodes that have ignore set to true
+		// relevant info: 
+		// - computed string as text
+		// - source
+		// - numberof children and ids
 		if !hasCurrentElement() {
 			return
 		}
