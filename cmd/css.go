45b83d6374e5f2d6b547b4129aa127ec46637d80
Guru Coyote <gc@testbild.tv>
2023-09-27 14:53:30 +0200

    aider: Implemented computed styles command in `cmd/css.go` to output the computed styles of the currently selected element as a JSON string.
    # Aider chat conversation:
    
    USER:
    Please see the code snippes in csscode.txt.
    Also see the examples in a11y.go for some usage of the proto package.
    Now please start implementing the computed styles command in cmd/css.go, output the result of the call as a JSON string for now
    
    ASSISTANT: To implement the computed styles command in `cmd/css.go`, we need to use the `CSSGetComputedStyleForNode` function from the `csscode.txt` file. This function will return the computed styles for a specified DOM node.
    
    We will need to modify the `cmd/css.go` file to implement this functionality.
    
    Here are the steps we will follow:
    
    1. Import the necessary packages to use the `CSSGetComputedStyleForNode` function.
    2. In the `Run` function of `ComputedStyleCmd`, get the `NodeID` of the currently selected element.
    3. Call the `CSSGetComputedStyleForNode` function with the `NodeID` as an argument.
    4. Handle any errors that may occur during the function call.
    5. If the function call is successful, output the result as a JSON string.
    
    Here is the *edit block* for the changes:
    
    ```go
    cmd/css.go
    <<<<<<< HEAD
    import (
    	"github.com/spf13/cobra"
    )
    
    var ComputedStyleCmd = &cobra.Command{
    	Use:   "computedstyles",
    	Short: "Output the computed styles of the currently selected element in a format suitable for a .css file",
    	Run: func(cmd *cobra.Command, args []string) {
    		// TODO: Implement command logic
    	},
    }
    
    func init() {
    	RootCmd.AddCommand(ComputedStyleCmd)
    }
    =======
    import (
    	"encoding/json"
    	"fmt"
    	"github.com/spf13/cobra"
    	"github.com/go-rod/rod/lib/proto"
    )
    
    var ComputedStyleCmd = &cobra.Command{
    	Use:   "computedstyles",
    	Short: "Output the computed styles of the currently selected element in a format suitable for a .css file",
    	Run: func(cmd *cobra.Command, args []string) {
    		if !hasCurrentElement() {
    			fmt.Println("No current element selected.")
    			return
    		}
    
    		// Get the element's properties
    		elementProperties, err := CurrentElement.Describe(0, false) // depth:0, pierce:false
    		if err != nil {
    			fmt.Println("Error describing element:", err)
    			return
    		}
    
    		// Call the CSSGetComputedStyleForNode function
    		computedStyle, err := proto.CSSGetComputedStyleForNode{
    			NodeID: elementProperties.NodeID,
    		}.Call(Page)
    		if err != nil {
    			fmt.Println("Error getting computed styles:", err)
    			return
    		}
    
    		// Output the result as a JSON string
    		computedStyleJSON, err := json.MarshalIndent(computedStyle, "", "  ")
    		if err != nil {
    			fmt.Println("Error converting computed styles to JSON:", err)
    			return
    		}
    		fmt.Println(string(computedStyleJSON))
    	},
    }
    
    func init() {
    	RootCmd.AddCommand(ComputedStyleCmd)
    }
    >>>>>>> updated
    ```
    
    Please note that this implementation assumes that the `CurrentElement` and `Page` variables are accessible in `cmd/css.go`, and that the `hasCurrentElement` function is defined and can be used to check if an element is currently selected. If these assumptions are not correct, additional changes may be needed.

--- cmd/css.go
+++ cmd/css.go
@@ -1,14 +1,44 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/go-rod/rod/lib/proto"
 )
 
 var ComputedStyleCmd = &cobra.Command{
 	Use:   "computedstyles",
 	Short: "Output the computed styles of the currently selected element in a format suitable for a .css file",
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Implement command logic
+		if !hasCurrentElement() {
+			fmt.Println("No current element selected.")
+			return
+		}
+
+		// Get the element's properties
+		elementProperties, err := CurrentElement.Describe(0, false) // depth:0, pierce:false
+		if err != nil {
+			fmt.Println("Error describing element:", err)
+			return
+		}
+
+		// Call the CSSGetComputedStyleForNode function
+		computedStyle, err := proto.CSSGetComputedStyleForNode{
+			NodeID: elementProperties.NodeID,
+		}.Call(Page)
+		if err != nil {
+			fmt.Println("Error getting computed styles:", err)
+			return
+		}
+
+		// Output the result as a JSON string
+		computedStyleJSON, err := json.MarshalIndent(computedStyle, "", "  ")
+		if err != nil {
+			fmt.Println("Error converting computed styles to JSON:", err)
+			return
+		}
+		fmt.Println(string(computedStyleJSON))
 	},
 }
 
